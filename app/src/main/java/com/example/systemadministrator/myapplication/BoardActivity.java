9a5999e9d487b166ee27d7c46ebb7232aa546a7a
Doug Whitley <dwhitley@pdx.edu>
2017-01-31 13:16:37 -0800

    Added new Board Activity and updated main Activity to update params

---
+++ app/src/main/java/com/example/systemadministrator/myapplication/BoardActivity.java
@@ -0,0 +1,99 @@
+package com.example.systemadministrator.myapplication;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.systemadministrator.Gomoku.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    //Working Board
+    int dimension = 15;
+    private ImageView[][] boardArray = new ImageView[dimension][dimension];
+    private Context context;
+    private int[][] piecesOnBoard = new int[dimension][dimension]; //0 is uninitialized, 1 or 2 to represent players pieces, 3 is empty
+    private Drawable[] drawCell = new Drawable[3];//0 is empty, 1 or 2 for different player pieces
+    private int xPos, yPos; // x and y position of move
+    private int playerTurn; // which players turn it is
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+        context = this;
+        playerTurn = 1;
+        loadResources();
+        createBoard();
+        init_board();
+    }
+
+    private void init_board() {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                boardArray[i][j].setImageDrawable(null);//clears content in all locations
+                piecesOnBoard[i][j] = 0;
+            }
+        }
+    }
+
+private void loadResources() {
+        drawCell[0] = context.getResources().getDrawable(R.drawable.plus, null);//empty cell
+        drawCell[1] = context.getResources().getDrawable(R.drawable.red_piece, null);//drawable for player 1
+        drawCell[2] = context.getResources().getDrawable(R.drawable.black_piece, null);//drawable for player 2
+    }
+
+    private void createBoard(){
+        int sizeofCell = Math.round(ScreenWidth() / dimension);
+
+        LinearLayout.LayoutParams singleCellDimensions = new LinearLayout.LayoutParams(sizeofCell, sizeofCell);
+        LinearLayout.LayoutParams singleRowDimensions = new LinearLayout.LayoutParams(sizeofCell * dimension, sizeofCell);
+        LinearLayout BoardGame = (LinearLayout) findViewById(R.id.Board);
+
+        //create cells
+        for (int i = 0; i < dimension; i++) {
+            LinearLayout newRow = new LinearLayout(context); //make a row
+            for (int j = 0; j < dimension; j++) { // loop through row and create individual cells
+                boardArray[i][j] = new ImageView(context);
+                boardArray[i][j].setBackground(drawCell[0]); //creates background + for all locations
+                final int x = i; //each location knows it x,y coordinates
+                final int y = j;
+                boardArray[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (piecesOnBoard[x][y] == 0) { // make sure cell is empty
+                            xPos = x;
+                            yPos = y; //when a location is clicked it sets global x,y variables to be accessed in other functions
+                            makeMove();
+                        }
+                    }
+                });
+                newRow.addView(boardArray[i][j], singleCellDimensions);
+            }
+            BoardGame.addView(newRow, singleRowDimensions); //add new row to board layout
+        }
+    }
+
+    private void makeMove() {
+        boardArray[xPos][yPos].setImageDrawable(drawCell[playerTurn]); //put players piece on space chosen
+        piecesOnBoard[xPos][yPos] = playerTurn; //keep track of board in 2d array data structure
+
+        if (playerTurn == 1) {
+            playerTurn = 2;
+        } else {
+            playerTurn = 1;
+        }
+    }
+
+    private float ScreenWidth() {
+        Resources resources = context.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return dm.widthPixels-100; //50 pixels of padding on each side
+    }
+}
--- app/src/main/java/com/example/systemadministrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/systemadministrator/myapplication/MainActivity.java
@@ -1,110 +1,61 @@
 package com.example.systemadministrator.myapplication;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
-import com.example.tanner.gomoku2.R;
+import com.example.systemadministrator.Gomoku.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
+        Potential object version
     private GameBoard playField;
     private Player player1;
     private Player player2;
+*/
 
-
-    //Working Board Import
-    int dimension = 15;
-    private ImageView[][] boardArray = new ImageView[dimension][dimension];
-    private Context context;
-    private int[][] piecesOnBoard = new int [dimension][dimension]; //0 is uninitialized, 1 or 2 to represent players pieces, 3 is empty
-    private Drawable[] drawCell = new Drawable[3];//0 is empty, 1 or 2 for different player pieces
-    private int xPos, yPos; // x and y position of move
-    private int playerTurn; // which players turn it is
-
-
+    private int players;
+    private int size;
+    private char style;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        players = 2;
+        size = 15;
+        style = 'c';
         setContentView(R.layout.activity_main);
     }
 
-    public void onPlayClick(View v){
-        //TODO update screen transition to appropriate grids
-        setContentView(R.layout.test_activity);
-        context = this;
-        playerTurn = 1;
-        loadResources();
-        createBoard();
-        init_board();
-    }
-    private void init_board(){
-        for (int i = 0; i < dimension; i++) {
-            for (int j = 0; j < dimension; j++) {
-                boardArray[i][j].setImageDrawable(null);//clears content in all locations
-                piecesOnBoard[i][j] = 0;
-            }
-        }
+    public void onPlayClick(View v) {
+        //TODO update screen transition to package the info gathered here.
     }
 
-    private void loadResources() {
-        drawCell[0] = context.getResources().getDrawable(R.drawable.plus, null);//empty cell
-        drawCell[1] = context.getResources().getDrawable(R.drawable.red_piece, null);//drawable for player 1
-        drawCell[2] = context.getResources().getDrawable(R.drawable.black_piece, null);//drawable for player 2
+    public void on1PlayerClick(View v) {
+        players = 1;
     }
 
-    private void createBoard(){
-        int sizeofCell = Math.round(ScreenWidth() / dimension);
-
-        LinearLayout.LayoutParams singleCellDimensions = new LinearLayout.LayoutParams(sizeofCell, sizeofCell);
-        LinearLayout.LayoutParams singleRowDimensions = new LinearLayout.LayoutParams(sizeofCell * dimension, sizeofCell);
-        LinearLayout BoardGame = (LinearLayout) findViewById(R.id.Board);
-
-        //create cells
-        for (int i = 0; i < dimension; i++) {
-            LinearLayout newRow = new LinearLayout(context); //make a row
-            for (int j = 0; j < dimension; j++) { // loop through row and create individual cells
-                boardArray[i][j] = new ImageView(context);
-                boardArray[i][j].setBackground(drawCell[0]); //creates background + for all locations
-                final int x = i; //each location knows it x,y coordinates
-                final int y = j;
-                boardArray[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (piecesOnBoard[x][y] == 0) { // make sure cell is empty
-                            xPos = x;
-                            yPos = y; //when a location is clicked it sets global x,y variables to be accessed in other functions
-                            makeMove();
-                        }
-                    }
-                });
-                newRow.addView(boardArray[i][j], singleCellDimensions);
-            }
-            BoardGame.addView(newRow, singleRowDimensions); //add new row to board layout
-        }
+    public void on2PlayerClick(View v) {
+        players = 2;
     }
 
-    private void makeMove() {
-        boardArray[xPos][yPos].setImageDrawable(drawCell[playerTurn]); //put players piece on space chosen
-        piecesOnBoard[xPos][yPos] = playerTurn; //keep track of board in 2d array data structure
-
-        if (playerTurn == 1) {
-            playerTurn = 2;
-        } else {
-            playerTurn = 1;
-        }
+    public void on10XClick(View v) {
+        size = 10;
     }
 
-    private float ScreenWidth() {
-        Resources resources = context.getResources();
-        DisplayMetrics dm = resources.getDisplayMetrics();
-        return dm.widthPixels-100; //50 pixels of padding on each side
+    public void on15XClick(View v){
+        size = 15;
     }
+
+    public void on20XClick(View v){
+        size = 20;
+    }
+
+    public void onClassicClick(View v) {
+        style = 'c';
+    }
+
+    public void onFreestyleClick(View v) {
+        style = 'f';
+    }
+
 }
