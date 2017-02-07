173ee6aa1ee6e9377d086a78e4e1a369d142814b
Ensath <nfreed4@gmail.com>
2017-02-07 13:27:34 -0800

    Added unit test for game logic

---
+++ app/src/test/java/com/example/systemadministrator/myapplication/GameLogicUnitTest.java
@@ -0,0 +1,18 @@
+package com.example.systemadministrator.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Noah Freed on 2/7/2017
+ * Local unit test for the game's rules and systems, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class GameLogicUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
