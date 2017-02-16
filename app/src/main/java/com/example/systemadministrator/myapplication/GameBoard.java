0cd2a09a9d8af6ac6961560e2a0152dd5557bbd2
Doug Whitley <dwhitley@pdx.edu>
2017-02-15 16:28:32 -0800

    Minor Logic fix for Game State

--- app/src/main/java/com/example/systemadministrator/myapplication/GameBoard.java
+++ app/src/main/java/com/example/systemadministrator/myapplication/GameBoard.java
@@ -217,7 +217,7 @@ public class GameBoard {
         if (playerNum == 0)
             return null;
 
-        if (start.x-1 < boardSize && start.y+1 < boardSize && board[start.x - 1][start.y + 1] == playerNum) {
+        if (start.x-1 > -1 && start.y+1 < boardSize && board[start.x - 1][start.y + 1] == playerNum) {
             Coordinates diaEnd = new Coordinates(start.x-1, start.y+1);
             int diaLength = 2;
             //find end of chain
@@ -243,7 +243,7 @@ public class GameBoard {
         if (toCheck.endPiece.x == boardSize - 1 && board[toCheck.startPiece.x - 1][toCheck.startPiece.y] == 0)
             return true;
         //*********internal to board case**********
-        if (toCheck.startPiece.x > 0 && toCheck.endPiece.x < boardSize-1 && board[toCheck.startPiece.x - 1][toCheck.startPiece.y] == 0 || board[toCheck.endPiece.x + 1][toCheck.endPiece.y] == 0)
+        if (toCheck.startPiece.x > 0 && toCheck.endPiece.x < boardSize-1 && (board[toCheck.startPiece.x - 1][toCheck.startPiece.y] == 0 || board[toCheck.endPiece.x + 1][toCheck.endPiece.y] == 0))
             return true;
         //Case: capped
         return false;
@@ -258,7 +258,7 @@ public class GameBoard {
         //Case: ends on edge of Board
         if(toCheck.endPiece.y == boardSize-1 && board[toCheck.startPiece.x][toCheck.startPiece.y-1] == 0) return true;
         //*********internal to board case**********
-        if(toCheck.startPiece.x > 1 && toCheck.endPiece.x < boardSize-1&& board[toCheck.startPiece.x-1][toCheck.startPiece.y] == 0 || board[toCheck.endPiece.x+1][toCheck.endPiece.y] == 0) return false;
+        if(toCheck.startPiece.x > 1 && toCheck.endPiece.x < boardSize-1&& (board[toCheck.startPiece.x-1][toCheck.startPiece.y] == 0 || board[toCheck.endPiece.x+1][toCheck.endPiece.y] == 0)) return false;
         //Case: capped
         return false;
     }
@@ -272,7 +272,7 @@ public class GameBoard {
         //Case: ends on edge of Board
         if (toCheck.endPiece.x == toCheck.endPiece.y && toCheck.endPiece.x == boardSize - 1 && board[toCheck.endPiece.x - 1][toCheck.endPiece.y - 1] == 0) return true;
         //*********internal to board case**********
-        if (toCheck.startPiece.x >0 && toCheck.startPiece.y > 0 && toCheck.endPiece.x < boardSize -1 || toCheck.endPiece.y < boardSize-1 && board[toCheck.startPiece.x - 1][toCheck.startPiece.y - 1] == 0 && board[toCheck.endPiece.x + 1][toCheck.endPiece.y + 1] == 0) return true;
+        if (toCheck.startPiece.x >0 && toCheck.startPiece.y > 0 && toCheck.endPiece.x < boardSize -1 && toCheck.endPiece.y < boardSize-1 && (board[toCheck.startPiece.x - 1][toCheck.startPiece.y - 1] == 0 || board[toCheck.endPiece.x + 1][toCheck.endPiece.y + 1] == 0)) return true;
         //Case: capped
         return false;
     }
@@ -286,7 +286,7 @@ public class GameBoard {
         //Case: ends on edge of Board
         if (toCheck.endPiece.x == 0 && toCheck.startPiece.y>0 && toCheck.startPiece.x < boardSize-1 && board[toCheck.startPiece.x +1 ][toCheck.startPiece.y - 1] == 0) return true;
         //*********internal to board case**********
-        if (toCheck.startPiece.x >0 && toCheck.startPiece.y > 0 && toCheck.endPiece.x < boardSize -1 || toCheck.endPiece.y < boardSize-1 && board[toCheck.startPiece.x - 1][toCheck.startPiece.y + 1] == 0 && board[toCheck.endPiece.x + 1][toCheck.endPiece.y + 1] == 0) return true;
+        if (toCheck.startPiece.x >0 && toCheck.startPiece.y > 0 && toCheck.endPiece.x < boardSize -1 && toCheck.endPiece.y < boardSize-1 && (board[toCheck.startPiece.x - 1][toCheck.startPiece.y + 1] == 0 || board[toCheck.endPiece.x + 1][toCheck.endPiece.y + 1] == 0)) return true;
         //Case: capped
         return false;
     }
