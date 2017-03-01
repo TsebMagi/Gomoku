0823ad383cc98fa1b8277991db4ef402558a054c
tanner9 <tanner9@pdx.edu>
2017-03-01 14:38:38 -0800

    Update network player
    Update network player to have places to store network data

--- app/src/main/java/com/example/systemadministrator/myapplication/NetworkPlayer.java
+++ app/src/main/java/com/example/systemadministrator/myapplication/NetworkPlayer.java
@@ -4,14 +4,15 @@ package com.example.systemadministrator.myapplication;
  * Created by Tanner on 2/6/2017.
  */
 
-public class NetworkPlayer extends HumanPlayer {
+ public class NetworkPlayer extends HumanPlayer {
     private Coordinates nextMove;
-    private BoardActivity arr;
+    private boolean madeMove;
 
     public NetworkPlayer(){
         super();
         this.nextMove = null;
         setGoesFirst();
+        madeMove = false;
     }
 
     public void sendMove(int xPos, int yPos){
@@ -19,9 +20,11 @@ public class NetworkPlayer extends HumanPlayer {
     }
 
     public Coordinates getNextMove(){
-
-
         return nextMove;
+    }
+
+    public void setNextMove(Coordinates nextMove){
+        this.nextMove = nextMove;
     }
 
     public void setNextMoveNull(){
@@ -30,5 +33,13 @@ public class NetworkPlayer extends HumanPlayer {
 
     public void setGoesFirst(){
         goesFirst = true;
+    }
+
+    public void setMadeMove(boolean madeMove){
+        this.madeMove = madeMove;
+    }
+
+    public boolean getMadeMove(){
+        return madeMove;
     }
 }
