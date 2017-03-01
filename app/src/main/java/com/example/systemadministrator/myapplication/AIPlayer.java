1d37ccfe3635960377978ba0e78bacbe087681f7
Doug Whitley <dwhitley@pdx.edu>
2017-03-01 15:58:41 -0800

    AI Update

--- app/src/main/java/com/example/systemadministrator/myapplication/AIPlayer.java
+++ app/src/main/java/com/example/systemadministrator/myapplication/AIPlayer.java
@@ -21,8 +21,33 @@ public class AIPlayer extends Player {
     public Coordinates generateMove(GameBoard board){
         ArrayList<Coordinates> toExamine = new ArrayList<>(board.OpenSpaces());
         StreakObj potentialWinStreak = board.getLongestWinnableStreak(playerNumber);
-        StreakObj potentialDefenseStreak = board.getLongestStreak((playerNumber+1)%2);
-        return new Coordinates(coor, coor++);
+        StreakObj potentialDefenseStreak = board.getLongestStreak((playerNumber-1));
+
+        //win
+        if(potentialWinStreak != null && potentialWinStreak.length == 3) {
+
+            for (Coordinates i : toExamine) {
+                if (potentialWinStreak.adjacentPoint(i))
+                    return i;
+            }
+        }
+
+        //defend
+        if(potentialDefenseStreak != null && potentialDefenseStreak.length == 3){
+            for(Coordinates i : toExamine)
+                if(potentialDefenseStreak.adjacentPoint(i))
+                    return i;
+        }
+
+        //add to longest streak
+        if(potentialWinStreak!=null){
+            for (Coordinates i : toExamine)
+                if (potentialWinStreak.adjacentPoint(i))
+                    return i;
+        }
+
+        //pick something
+        return toExamine.get(0);
     }
 
     public Coordinates generateFreestyleMove(GameBoard board){return new Coordinates(coor, coor++); }
